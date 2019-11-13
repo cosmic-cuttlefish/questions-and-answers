da6992a10ae0588aae5bbfe4ea04f7b23c55e605
cferdinand <chrisferdinand07@gmail.com>
2019-11-12 21:12:43 -0500

    working test coverage and load testing

--- __tests__/questionsAndAnswers.test.js
+++ __tests__/questionsAndAnswers.test.js
@@ -1,5 +1,31 @@
+const frisby = require("frisby");
+const Joi = frisby.Joi;
+const { getQuestions } = require("../server/models/questionsModels.js");
+
 describe("practice test", () => {
-  it("this is a test", () => {
-    expect(true).toBe(true);
+  it("should get a list of questions from the database", async () => {
+    const id = 1;
+    const questionId = 5;
+
+    let output;
+
+    await getQuestions(id).then(data => {
+      output = data.rows[0].id;
+    });
+    expect(output).toBe(questionId);
+  });
+
+  it("should return a status of 200 when sending a request for questions with the correct format for the questions", () => {
+    return frisby
+      .get("http://localhost:3000/qa/1/questions")
+      .expect("status", 200)
+      .expect("jsonTypes", "rows.*", {
+        id: Joi.number(),
+        question_body: Joi.string()
+      });
   });
 });
+
+// it("should return an empty array when no questions are found", () => {});
+
+// it("should return an array of photos", () => {});

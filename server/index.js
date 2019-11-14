29c86c19ab3edc3b33938ed49baf5012e1882f28
cferdinand <chrisferdinand07@gmail.com>
2019-11-13 23:34:52 -0500

    unable to connect my database in docker but all data is loaded into the docker container

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyparser = require("body-parser");
 const routes = require("./routes/qandaRoutes.js");
+require("dotenv").config();
 const port = process.env.PORT || 3000;
 
 //app.use(express.static("./client/dist"));

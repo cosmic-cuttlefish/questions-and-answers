1161245429821f39300ac7469761d3ba3be0b350
cferdinand <chrisferdinand07@gmail.com>
2020-04-01 23:46:16 -0400

    fixed load testing endpoint

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyparser = require("body-parser");
 const routes = require("./routes/qandaRoutes.js");
+const path = require("path");
 const port = process.env.PORT || 3000;
 
 //app.use(express.static("./client/dist"));
@@ -11,8 +12,9 @@ app.use(bodyparser.json());
 app.use("/qa", routes);
 
 app.get("/loaderio*", (req, res) => {
-  let file = req.path.substr(1);
-  res.status(200).sendFile(__dirname + `/loaderio/${file}.txt`);
+  let file = req.path.substr(1, req.path.length - 2);
+  let filePath = path.resolve(`server/loaderio/${file}.txt`);
+  res.status(200).sendFile(filePath);
 });
 
 app.listen(port, () => console.log(`App is listening on port ${port}!`));
